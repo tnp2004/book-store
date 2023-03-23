493457f3f4865b222c8caf91d44fa4d28369c663
tnp2004 <iceandiceisice2004@gmail.com>
2023-03-23 15:48:01 +0700

    Set images patterns  and Update dynamic search by path

--- src/pages/api/getBooks.ts
+++ src/pages/api/getBooks.ts
@@ -1,11 +1,12 @@
 import { NextApiRequest, NextApiResponse } from "next"
+import { useRouter } from "next/router"
 
 export default async function getBooks(req: NextApiRequest, res: NextApiResponse) {
     try {
-        const key = 'AIzaSyDoZVy32LvDQv9NWvqqmVdrrUfSJslQXxc'
-        // console.log(req.query)
-        const search = 'cat'
-        const data = await (await (fetch(`https://www.googleapis.com/books/v1/volumes?q=${search}&key=${key}`))).json()
+        const key = process.env.BOOK_API_KEY
+        const searchDefault = 'cat'
+        const { search } = req.query
+        const data = await (await (fetch(`https://www.googleapis.com/books/v1/volumes?q=${search ? search : searchDefault}&key=${key}`))).json()
 
         res.json(data)
 
--- src/pages/library/[search].tsx
+++ src/pages/library/[search].tsx
@@ -1,23 +1,28 @@
 import Layout from 'components/Layout'
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import { BooksObj, Item } from 'types/types'
 import Image from 'next/image'
+import { useRouter } from 'next/router'
 
 type Props = {
     data: BooksObj
 }
 
-export default function Index({ data }: Props) {
+export default function Search({ data }: Props) {
 
     const [booksData, setBooksData] = useState<Item[]>(data.items)
+    const router = useRouter()
+    const { search } = router.query
+    const [searchWord, setSearchWord] = useState(search)
 
     return (
         <Layout>
             <div className="text-center">
                 <h1 className='text-rose-500 text-3xl'>Library page</h1>
+                {searchWord}
                 <div className='p-2 group w-max mx-auto'>
                     <input type="text" className='border-2 border-r-0 group-hover:border-slate-800 rounded-l p-1' placeholder='search' />
-                    <button className='border-2 border-l-0 group-hover:border-slate-800 p-1 bg-sky-300 rounded-r'>search</button>
+                    <a className='border-2 border-l-0 group-hover:border-slate-800 p-1 bg-sky-300 rounded-r'>search</a>
                 </div>
                 <ul>
                     {booksData.map((val: Item, index: number) => (
@@ -31,9 +36,9 @@ export default function Index({ data }: Props) {
     )
 }
 
-export async function getStaticProps() {
+export async function getStaticProps({params}: any) {
 
-    const data = await (await fetch('http://localhost:3000/api/getBooks')).json()
+    const data = await (await fetch(`http://localhost:3000/api/getBooks?search=${params?.search}`)).json()
 
     return {
         props: {
@@ -41,3 +46,14 @@ export async function getStaticProps() {
         }
     }
 }
+
+export async function getStaticPaths() {
+    
+    const key = process.env.BOOK_API_KEY
+    // const data = await (await (fetch(`https://www.googleapis.com/books/v1/volumes?q=${search}&key=${key}`))).json()
+
+    return {
+      paths: [{ params: { search: 'bird' } }],
+      fallback: false,
+    }
+  }
