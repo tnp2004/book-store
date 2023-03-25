2bbc14199928e3f0feb30c83a570da4eb97f8634
tnp2004 <iceandiceisice2004@gmail.com>
2023-03-25 13:20:12 +0700

    Search element keyboard event
    search when pressed 'Enter' button

--- src/pages/library/index.tsx
+++ src/pages/library/index.tsx
@@ -1,5 +1,5 @@
 import Layout from 'components/Layout'
-import React, { useState } from 'react'
+import React, { KeyboardEvent, useState } from 'react'
 import { BooksObj, Item } from 'types/types'
 import Book from 'components/Book'
 
@@ -10,7 +10,7 @@ type Props = {
 export default function Index({ data }: Props) {
 
     const [booksData, setBooksData] = useState<Item[]>(data.items)
-    const [searchValue, SetSearchValue] = useState('')
+    const [searchValue, SetSearchValue] = useState<string>('')
 
 
     const searchBooks = async () => {
@@ -18,22 +18,27 @@ export default function Index({ data }: Props) {
         setBooksData(data.items)
     }
 
+    const isPressEnter = (event: KeyboardEvent, triggerFunc: Function) => {
+        if (event.key === 'Enter') {
+            triggerFunc()
+        }
+    }
+
     return (
         <Layout>
             <div className="text-center">
                 <h1 className='text-rose-500 text-3xl'>Library page</h1>
                 <div className='p-2 group w-max mx-auto'>
-                    <input type="text" className='border-2 border-r-0 group-hover:border-slate-800 rounded-l p-1' placeholder='search' onChange={(e) => SetSearchValue(e.target.value)} value={searchValue} />
-                    <button className='border-2 border-l-0 group-hover:border-slate-800 p-1 bg-sky-300 rounded-r' onClick={searchBooks}>search</button>
+                    <input onKeyDown={(e) => isPressEnter(e, searchBooks)} id='search' type="text" className='border-2 group-hover:border-slate-800 rounded p-1' placeholder='search' onChange={(e) => SetSearchValue(e.target.value)} value={searchValue} />
                 </div>
                 {booksData ? (
                     //  have data
                     <div className='flex flex-wrap justify-center gap-12 mt-32'>
                         {booksData.map((book: Item, index: number) => (
-                           <Book key={index} book={book} />
+                            <Book key={index} book={book} />
                         ))}
                     </div>
-                ): (
+                ) : (
                     // no data
                     <div>no data</div>
                 )}
